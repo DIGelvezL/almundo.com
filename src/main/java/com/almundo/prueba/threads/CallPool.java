12d7eb2f0240f9600bcb6c40453eda1259c9e093
Jefferson perez cervera <jperezc@intergrupo.com>
2018-02-13 15:20:50 -0500

    add singleton pool

---
+++ src/main/java/com/almundo/prueba/threads/CallPool.java
@@ -0,0 +1,18 @@
+package com.almundo.prueba.threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CallPool {
+	
+	private static ExecutorService executor;
+	
+	public static ExecutorService getExecutorService() {
+		if(executor==null) {
+			executor=Executors.newFixedThreadPool(10);
+		}
+		return executor;
+	}
+	
+	
+}
